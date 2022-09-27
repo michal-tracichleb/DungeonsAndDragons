92306d5618c2f65674c14c0fa542b0deb9f9460d
Michał Tracichleb <Michal.Tracichleb@gmail.com>
2022-09-27 23:32:18 +0200

    Added function with choice race

--- DungeonsAndDragons/CharacterCreator.cs
+++ DungeonsAndDragons/CharacterCreator.cs
@@ -8,12 +8,18 @@ namespace DungeonsAndDragons
             Console.Clear();
             Console.WriteLine("Znajdujesz się w kreatorze bohatera.");
             Console.WriteLine("Postępuj zgodnie z poleceniami, aby stworzyć swoją postać :)");
-            Console.WriteLine("Zacznijmy od czegoś prostego, wybierz swoją rasę wybierając odpowiedni numer:");
+            Console.WriteLine("Zacznijmy od czegoś prostego, wybierz swoją rasę.");
+            Console.WriteLine("Gotowy? Wciśnij cokolwiek na klawiarze lub ESC by wyjść do menu.");
 
-            ChoiceRace.Race();
+            ConsoleKey choice = Console.ReadKey().Key;
+
+            if (choice == ConsoleKey.Escape) return;
+
+            int idRace = ChoiceRace.Race();
+
+            Console.WriteLine($"Wybrałeś {idRace}");
 
             Console.ReadKey();
-
         }
     }
 }
--- DungeonsAndDragons/CharacterCreator/ChoiceRace.cs
+++ DungeonsAndDragons/CharacterCreator/ChoiceRace.cs
@@ -2,46 +2,56 @@
 using DungeonsAndDragons.ListOfRaces;
 using Newtonsoft.Json;
 using System;
+using System.Diagnostics;
 
 namespace DungeonsAndDragons
 {
     public class ChoiceRace
     {
-        public static void Race()
+        public static int Race()
         {
+            
             var currentDirectory = Directory.GetCurrentDirectory();
             var racesSerializedPath = Path.Combine(currentDirectory, "data", "racesPL.json");
             string racesSerialized = File.ReadAllText(racesSerializedPath);
-
             Races races = JsonConvert.DeserializeObject<Races>(racesSerialized);
 
-            int idRace = -1;
-            foreach (var race in races.ListOfRaces)
+            int idRace = 0;
+
+            do
             {
-                idRace += 1;
+                bool again = false;
+                Console.Clear();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("=======================================================================");
+                Console.WriteLine("Jeśli zdecydujesz się na poniższą rasę wciśnij ENTER lub ESC aby wyjść.");
+                Console.WriteLine("Następny wybór, wciśnij strzałkę w prawo");
+                Console.WriteLine("Poprzedni wybór, wciśnij strzałkę w lewo");
+                Console.WriteLine("=======================================================================\n");
+                Console.ResetColor();
+
                 Console.Write($"{idRace}. ");
                 Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine(race.Name);
+                Console.WriteLine(races.ListOfRaces[idRace].Name);
                 Console.ResetColor();
-                Console.WriteLine(race.Description);
+                Console.WriteLine(races.ListOfRaces[idRace].Description);
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(race.AbilityScoreDescription);
+                Console.WriteLine(races.ListOfRaces[idRace].AbilityScoreDescription);
                 Console.ResetColor();
-                Console.WriteLine(race.SpeedDescription);
+                Console.WriteLine(races.ListOfRaces[idRace].SpeedDescription);
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.WriteLine($"Biegłość:");
                 Console.ResetColor();
-                Console.WriteLine($"{race.Traits}\n");
+                Console.WriteLine($"{races.ListOfRaces[idRace].Traits}\n");
 
-
-
-                if (race.Subraces.Count != 0)
+                if (races.ListOfRaces[idRace].Subraces.Count != 0)
                 {
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;                    
-                    Console.WriteLine("\nWystępują podrasy: ");
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("Występują podrasy: ");
                     Console.ResetColor();
 
-                    foreach (var subRace in race.Subraces)
+                    foreach (var subRace in races.ListOfRaces[idRace].Subraces)
                     {
                         Console.ForegroundColor = ConsoleColor.DarkMagenta;
                         Console.WriteLine(subRace.Name);
@@ -56,8 +66,43 @@ namespace DungeonsAndDragons
                         Console.WriteLine($"{subRace.Traits}\n");
                     }
                 }
-            }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("=======================================================================");
+                Console.WriteLine("Jeśli zdecydujesz się na poniższą rasę wciśnij ENTER lub ESC aby wyjść.");
+                Console.WriteLine("Następny wybór, wciśnij strzałkę w prawo");
+                Console.WriteLine("Poprzedni wybór, wciśnij strzałkę w lewo");
+                Console.WriteLine("=======================================================================\n");
+                Console.ResetColor();
 
+                do
+                {
+                    ConsoleKey choice = Console.ReadKey().Key;
+
+                    switch (choice)
+                    {
+                        case ConsoleKey.RightArrow:
+                            if (idRace != (races.ListOfRaces.Count - 1)) idRace++;
+                            else idRace = 0;
+                            again = false;
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            if (idRace != 0) idRace--;
+                            else idRace = (races.ListOfRaces.Count - 1);
+                            again = false;
+                            break;
+                        case ConsoleKey.Enter:
+                            return idRace;
+                        case ConsoleKey.Escape:
+                            return 999;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\nNieznana operacja, wybierz jeszcze raz.");
+                            Console.ResetColor();
+                            again = true;
+                            break;
+                    }
+                }while (again);
+            } while (true);
         }
     }
 }
